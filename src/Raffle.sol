0d99ed1652686f75d35d28136a6d8ee6855a1763
doubleb219 <brownsbob56@gmail.com>
2025-02-19 11:08:11 -0800

    chore: forge init

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.19;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+
+
 
 /** 
  * @title Raffle contract
