0403456930c09649a0dc3317590fde68cdda605a
doubleb219 <brownsbob56@gmail.com>
2025-02-21 03:36:50 -0800

    add deployRaffle script

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+
+contract DeployRaffle is Script {
+    function run() public {}
+
+    // function deployContract() public returns(Raffle, HelperConfig) {}
+}
